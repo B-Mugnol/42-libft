58469090673249c513fc44c22e3dfc7531b5fb28
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-26 14:26:11 -0300

    Realized the i variable was unnecessary and removed it

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 13:21:13 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/24 14:23:34 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/26 14:24:49 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
 	char	cast_c;
 
 	cast_c = (char)(c);
-	i = 0;
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		if (*(s + i) == cast_c)
-			return ((char *)(s + i));
-		i++;
+		if (*s == cast_c)
+			return ((char *)(s));
+		s++;
 	}
 	if (cast_c == '\0')
-		return ((char *)(s + i));
+		return ((char *)(s));
 	return (NULL);
 }
