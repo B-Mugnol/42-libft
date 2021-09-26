947ee864ddb10bc2f372db13babe5c86d5d922aa
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-26 14:47:52 -0300

    changed casting to be done to entire string instead of every value inside the While loop

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 15:23:16 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/24 15:42:43 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/26 14:48:14 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*cast_s1;
+	unsigned char	*cast_s2;
 
+	cast_s1 = (unsigned char *)(s1);
+	cast_s2 = (unsigned char *)(s2);
 	if (size == 0)
 		return (0);
 	i = 0;
-	while (i < size && *(s1 + i) != '\0' && *(s2 + i) != '\0')
+	while (i < size && *(cast_s1 + i) != '\0' && *(cast_s2 + i) != '\0')
 	{
-		if (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i) != 0)
-			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		if (*(cast_s1 + i) - *(cast_s2 + i) != 0)
+			return (*(cast_s1 + i) - *(cast_s2 + i));
 		i++;
 	}
-	if (*(s1 + i) == '\0' || *(s2 + i) == '\0')
-		return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+	if (*(cast_s1 + i) == '\0' || *(cast_s2 + i) == '\0')
+		return (*(cast_s1 + i) - *(cast_s2 + i));
 	return (0);
 }
