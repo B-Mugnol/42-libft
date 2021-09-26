14a01d59463ec5c0ded6669132797b3420bfec41
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-26 14:21:29 -0300

    Changed pointer type to avoid char vs unsigned char contradiction

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 16:26:01 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/23 21:22:06 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/26 14:20:52 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	*ft_memset(void *str, int c, size_t size)
 {
-	char			*p;
+	unsigned char	*p;
 	unsigned char	cast_c;
 
-	p = (char *)(str);
+	p = (unsigned char *)(str);
 	cast_c = (unsigned char)(c);
 	while (size > 0)
 	{
