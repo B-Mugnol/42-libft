479ce72e90d4f7a1d4f6484800622d7a57c894bd
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-26 16:03:07 -0300

    calloc using ft_bzero

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 15:33:44 by bmugnol-          #+#    #+#             */
+/*   Updated: 2021/09/26 15:58:23 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	ft_bzero(p, nmemb * size);
+	return (p);
+}
