d57faeb8155fff776bc66c88f49d96febc6adf45
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-27 17:31:23 -0300

    a parameter with value 0 now returns a pointer to NULL instead of NULL directly

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:33:44 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/27 17:22:27 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/27 17:30:20 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	p = malloc(nmemb * size);
+	if (size == 0)
+		return (p);
 	if (p == NULL)
 		return (NULL);
 	ft_bzero(p, nmemb * size);
