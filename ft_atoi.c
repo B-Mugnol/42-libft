e7b643b38f8261fa7fac0d53ac533f7000c27554
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-27 23:59:26 -0300

    git giveup (on under/overflows in atoi)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:08:00 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/27 23:47:11 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/27 23:58:44 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,21 @@ static int	ft_isspace(char c)
 
 int	ft_atoi(const char *s)
 {
-	unsigned int	num;
-	int				sign;
-	int				count;
+	int	num;
+	int	sign;
 
 	num = 0;
-	count = 0;
 	sign = 1;
 	while (ft_isspace(*s))
 		s++;
 	if (*s == '-')
-	{
 		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
+	if (*s == '+' || *s == '-')
 		s++;
 	while (*s >= '0' && *s <= '9')
 	{
 		num = 10 * num + *s - '0';
-		count++;
 		s++;
 	}
-	if (count == 10 && num <= 2147483647)
-		return (num * sign);
-	if (count >= 10 && sign == 1)
-		return (2147483647);
-	if (count >= 10 && sign == -1)
-		return (-2147483648);
 	return (num * sign);
 }
