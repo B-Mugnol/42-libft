70c9076cb02cb51dc9a83c5fd861951fb1a9da95
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-27 22:25:32 -0300

    Revert "added overflow and underflow treatment"
    This reverts commit fe228ff12142b69a859e3a61680975132cc1ec1d.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:08:00 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/27 22:23:39 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:40:48 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,6 @@ int	ft_atoi(const char *s)
 	while (*s >= '0' && *s <= '9')
 	{
 		num = 10 * num + *s - '0';
-		if (num * 10 + *(s + 1) < 0 && sign == 1)
-			return (2147483647);
-		if (num * 10 + *(s + 1) < 0 && sign == -1)
-			return (-2147483648);
 		s++;
 	}
 	return (num * sign);
