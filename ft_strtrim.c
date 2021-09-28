31654e8144561f9cf2c0d8be020692733746ddd8
bmugnol- <bmugnol-@student.42sp.org.br>
2021-09-28 13:23:17 -0300

    strtrim done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:24:16 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/27 17:49:31 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/09/28 13:22:35 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s2;
 	char	*start;
 	char	*end;
-	size_t	set_len;
 
+	if (ft_strlen(set) == 0)
+		return (ft_substr(s1, 0, ft_strlen(s1)));
 	start = (char *)(s1);
-	end = (char *)(s1) + ft_strlen(s1);
-	set_len = ft_strlen(set);
-	if (ft_strnstr(start, set, set_len))
-		start = start + set_len;
-	if (ft_strnstr(end - set_len, set, set_len))
-		end = end - set_len;
-	s2 = ft_calloc(end - start + 1, sizeof (char));
-	if (s2 == NULL)
-		return (s2);
-	ft_strlcpy(s2, start, end - start + 1);
-	return (s2);
+	end = (char *)(s1 + ft_strlen(s1));
+	while (ft_strchr(set, *start) != NULL && *start != '\0')
+		start++;
+	while (ft_strchr(set, *end) != NULL && end > start)
+		end--;
+	return (ft_substr(start, 0, end - start + 1));
 }
