edab17582afca372b467f938459d3ba681a7accd
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-01 17:19:59 -0300

    lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 16:08:09 by bmugnol-          #+#    #+#             */
+/*   Updated: 2021/10/01 17:18:03 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (!lst)
+		return ;
+	(*del)((*lst).content);
+	temp = (*lst).next;
+	free(lst);
+	lst = temp;
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 17:45:45 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/01 15:03:33 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/01 16:08:46 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,11 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
