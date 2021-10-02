4cbe83ed151fbeb24fa477f5f708b7fa4306ccd3
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-02 20:27:52 -0300

    now checks if del function exists

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 16:08:09 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/01 17:18:03 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/02 20:23:59 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	t_list	*temp;
-
-	if (!lst)
+	if (!lst || !del)
 		return ;
 	(*del)((*lst).content);
-	temp = (*lst).next;
 	free(lst);
-	lst = temp;
 }
