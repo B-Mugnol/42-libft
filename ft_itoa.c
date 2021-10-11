6976d7a4c2a6b8d4737415be37412189362ea0e8
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-11 17:56:49 -0300

    changed to a less complex solution

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 11:39:53 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/11 17:30:59 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/11 17:53:41 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,6 @@ static int	count_digits(int n)
 	return (digit_count);
 }
 
-static int	ft_pow(int x, int y)
-{
-	int	result;
-
-	if (x == 0 && y < 0)
-		return (0);
-	result = 1;
-	while (y > 0)
-	{
-		result *= x;
-		y--;
-	}
-	while (y < 0 && x)
-	{
-		result /= x;
-		y++;
-	}
-	return (result);
-}
-
 static int	ft_abs(int x)
 {
 	if (x < 0)
@@ -54,24 +34,11 @@ static int	ft_abs(int x)
 	return (x);
 }
 
-static void	populate_string(int n, int digit_count, char *s, unsigned int index)
-{
-	while (digit_count != 0)
-	{
-		s[index] = ft_abs(n / ft_pow(10, digit_count - 1)) + '0';
-		n = n % ft_pow(10, digit_count - 1);
-		digit_count--;
-		index++;
-	}
-	s[index] = '\0';
-}
-
 char	*ft_itoa(int n)
 {
 	char	*s;
 	int		is_negative;
 	int		digit_count;
-	int		i;
 
 	is_negative = 0;
 	digit_count = count_digits(n);
@@ -80,12 +47,14 @@ char	*ft_itoa(int n)
 	s = malloc((digit_count + is_negative + 1) * sizeof (char));
 	if (!s)
 		return (NULL);
-	i = 0;
 	if (is_negative)
-	{
 		s[0] = '-';
-		i++;
+	*(s + digit_count + is_negative) = '\0';
+	while (digit_count > 0)
+	{
+		*(s + digit_count - 1 + is_negative) = ft_abs(n % 10) + '0';
+		n /= 10;
+		digit_count--;
 	}
-	populate_string(n, digit_count, s, i);
 	return (s);
 }
