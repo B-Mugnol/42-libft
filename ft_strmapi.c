06447d52ff3bb78e8d4972b59b6d9cd49c7f67fa
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-11 18:36:15 -0300

    replaced calloc with malloc and changed callback function syntax to a simpler one

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 13:28:47 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/01 14:27:36 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/11 18:35:21 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	if (!s)
 		return (NULL);
-	dest = ft_calloc(ft_strlen(s) + 1, sizeof (char));
+	dest = malloc((ft_strlen(s) + 1) * sizeof (char));
 	if (!dest)
 		return (NULL);
 	i = 0;
 	while (*(s + i) != '\0')
 	{
-		*(dest + i) = (*f)(i, *(s + i));
+		*(dest + i) = f(i, *(s + i));
 		i++;
 	}
 	*(dest + i) = '\0';
