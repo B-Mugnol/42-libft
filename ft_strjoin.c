f815e44556d053ba9a0fe2493a1c4f92722ed763
bmugnol- <bmugnol-@student.42sp.org.br>
2021-10-11 18:32:55 -0300

    replaced function calls with code

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:09:11 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/11 18:24:07 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/10/11 18:33:03 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*s3;
-	size_t	size;
+	size_t	i;
 
 	if (!s1 || !s2)
 		return (NULL);
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	s3 = malloc(size * sizeof (char));
+	s3 = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof (char));
 	if (!s3)
 		return (NULL);
-	ft_strlcpy(s3, s1, size);
-	ft_strlcat(s3, s2, size);
+	i = 0;
+	while (*s1 != '\0')
+		*(s3 + i++) = *s1++;
+	while (*s2 != '\0')
+		*(s3 + i++) = *s2++;
+	*(s3 + i) = '\0';
 	return (s3);
 }
