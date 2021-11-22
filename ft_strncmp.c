a44d3216b240c9b9c866630d01d70e04b75bfdf6
B-Mugnol <bruno_mugnol@hotmail.com>
2021-11-22 16:57:35 +0100

    missing * meant function didn't stop at \0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 15:23:16 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/11 18:39:15 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2021/11/22 16:56:28 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	if (size == 0)
 		return (0);
 	i = 0;
-	while (i < size - 1 && *(cast_s1 + i) == *(cast_s2 + i) && (cast_s1 + i))
+	while (i < size - 1 && *(cast_s1 + i) == *(cast_s2 + i) && *(cast_s1 + i))
 		i++;
 	return (*(cast_s1 + i) - *(cast_s2 + i));
 }
