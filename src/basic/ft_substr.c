58802c12403e2929a438042118601663e663cbad
B-Mugnol <bruno_mugnol@hotmail.com>
2022-01-28 23:09:07 +0100

    fixed extra ',' in malloc

--- src/basic/ft_substr.c
+++ src/basic/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 13:28:33 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/01/28 23:02:55 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/01/28 23:07:35 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	if (s_len - start < len)
 		len = s_len - start;
-	substr = malloc(len + 1, sizeof (char));
+	substr = malloc((len + 1) * sizeof (char));
 	if (!substr)
 		return (NULL);
 	ft_strlcpy(substr, s + start, len + 1);
