68bd8b65a861ca1ec0b5667d3a4df46900e055af
B-Mugnol <bruno_mugnol@hotmail.com>
2022-03-29 22:10:21 +0200

    changed param names

--- src/basic/ft_partial_split.c
+++ src/basic/ft_partial_split.c
@@ -6,17 +6,17 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 22:19:43 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/03/15 01:18:37 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/03/29 22:09:59 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t		count_words(char const *s, char delimiter, char stopper);
-static char const	*separator(char const *s, char **dest, char delimiter,
-						char stopper);
+static size_t		count_words(char const *s, char splitter, char delimiter);
+static char const	*separator(char const *s, char **dest, char splitter,
+						char delimiter);
 
-char	**ft_partial_split(char const *s, char delimiter, char stopper)
+char	**ft_partial_split(char const *s, char splitter, char delimiter)
 {
 	char	**dest;
 	size_t	i;
@@ -24,14 +24,14 @@ char	**ft_partial_split(char const *s, char delimiter, char stopper)
 
 	if (!s)
 		return (NULL);
-	dest = malloc((count_words(s, delimiter, stopper) + 1) * sizeof (char *));
+	dest = malloc((count_words(s, splitter, delimiter) + 1) * sizeof (char *));
 	if (!dest)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i] != '\0' && j < count_words(s, delimiter, stopper))
+	while (s[i] != '\0' && j < count_words(s, splitter, delimiter))
 	{
-		i = separator(s + i, dest + j, delimiter, stopper) - s;
+		i = separator(s + i, dest + j, splitter, delimiter) - s;
 		if (!dest[j])
 		{
 			ft_free_matrix((void *)(&dest), j);
@@ -43,7 +43,7 @@ char	**ft_partial_split(char const *s, char delimiter, char stopper)
 	return (dest);
 }
 
-static size_t	count_words(char const *s, char delimiter, char stopper)
+static size_t	count_words(char const *s, char splitter, char delimiter)
 {
 	size_t	word_count;
 	size_t	i;
@@ -53,40 +53,40 @@ static size_t	count_words(char const *s, char delimiter, char stopper)
 	word_count = 0;
 	while (s[++i] != '\0')
 	{
-		if (s[i] == stopper)
+		if (s[i] == delimiter)
 		{
 			word_count++;
 			j = 1;
-			while (s[i + j] != stopper && s[i + j] != '\0')
+			while (s[i + j] != delimiter && s[i + j] != '\0')
 				j++;
-			if (i > 0 && s[i - 1] != delimiter && s[i + j] != '\0')
+			if (i > 0 && s[i - 1] != splitter && s[i + j] != '\0')
 				word_count++;
 			i += j;
 			if (s[i] == '\0')
 				break ;
 		}
-		else if (s[i] != delimiter && (s[i + 1] == delimiter
+		else if (s[i] != splitter && (s[i + 1] == splitter
 				|| s[i + 1] == '\0'))
 			word_count++;
 	}
 	return (word_count);
 }
 
-static char const	*separator(char const *s, char **dest, char delimiter,
-	char stopper)
+static char const	*separator(char const *s, char **dest, char splitter,
+	char delimiter)
 {
 	size_t	i;
 	size_t	k;
 
 	i = 0;
-	while (s[i] == delimiter && s[i] != '\0')
+	while (s[i] == splitter && s[i] != '\0')
 		i++;
 	if (s[i] == '\0')
 		return (s + i);
 	k = 0;
-	if (s[i] == stopper)
+	if (s[i] == delimiter)
 	{
-		while ((s[i + k] != stopper && s[i + k] != '\0') || k == 0)
+		while ((s[i + k] != delimiter && s[i + k] != '\0') || k == 0)
 			k++;
 		if (k == 1)
 			*dest = ft_substr(s, i, k);
@@ -96,7 +96,7 @@ static char const	*separator(char const *s, char **dest, char delimiter,
 			*dest = ft_substr(s, ++i, k - 1);
 		return (s + i + k);
 	}
-	while (s[i + k] != delimiter && s[i + k] != stopper && s[i + k] != '\0')
+	while (s[i + k] != splitter && s[i + k] != delimiter && s[i + k] != '\0')
 		k++;
 	*dest = ft_substr(s, i, k);
 	return (s + i + k);
