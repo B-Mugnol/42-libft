d1852e9c7d4f33858f63b2369826d16d4f90a18c
B-Mugnol <bruno_mugnol@hotmail.com>
2022-07-13 02:10:49 +0200

    added number*size being bigger than allowed verification

--- src/stdlib/ft_calloc.c
+++ src/stdlib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:33:44 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/09/29 22:28:35 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/07/13 02:10:03 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
+	if (nmemb >= __SIZE_MAX__ / size)
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 		return (p);
