f27fce8af8199bf8af84edbec7ea2f809e3d1f90
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-27 00:50:23 +0200

    improved readability

--- src/stdlib/ft_atoi.c
+++ src/stdlib/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:08:00 by bmugnol-          #+#    #+#             */
-/*   Updated: 2021/10/11 17:20:48 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/27 00:49:09 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int	ft_atoi(const char *s)
 
 	num = 0;
 	sign = 1;
-	while (*s == 32 || (*s >= 9 && *s <= 13))
+	while (ft_isspace(*s))
 		s++;
-	if (*s == 45)
+	if (*s == '-')
 		sign = -1;
-	if (*s == 43 || *s == 45)
+	if (*s == '-' || *s == '+')
 		s++;
-	while (*s >= 48 && *s <= 57)
+	while (ft_isdigit(*s))
 	{
-		num = 10 * num + *s - 48;
+		num = 10 * num + *s - ('0' - 0);
 		s++;
 	}
 	return (num * sign);
